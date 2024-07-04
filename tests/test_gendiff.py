ed4e4465f4b0b71ad9a0220b6239fd8478dae3ef
DmitryFedoreev <fedoreev.doc@gmail.com>
2024-07-04 13:25:07 +0500

    Fixed an error with the tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,17 +1,14 @@
 import pytest
-import json
 
 
 @pytest.fixture
 def file_paths():
-    file1_path = '/home/dmitry/PycharmProjects/python-project-50/file1.json'
-    file2_path = '/home/dmitry/PycharmProjects/python-project-50/file2.json'
+    file1_path = 'file1.json'
+    file2_path = 'file2.json'
     return file1_path, file2_path
 
 
 def test_generate_diff(file_paths):
     file1_path, file2_path = file_paths
 
-    with open(file1_path) as f1, open(file2_path) as f2:
-        data1 = json.load(f1)
-        data2 = json.load(f2)
+
